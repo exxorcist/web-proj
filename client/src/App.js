f8b24a915c97962aea55b0b489db0b35108eaa22
exxorcist <102036729+exxorcist@users.noreply.github.com>
2022-12-10 18:28:32 +0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
 import {
   BrowserRouter as Router,
-  Routes,
+  Switch,
   Route,
-  Navigate,
+  Redirect,
 } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
@@ -15,21 +15,20 @@ function App() {
   const { user } = useContext(AuthContext);
   return (
     <Router>
-      <Routes>
+      <Switch>
         <Route exact path="/">
           {user ? <Home /> : <Register />}
         </Route>
-        <Route path="/login">{user ? <Navigate to="/" /> : <Login />}</Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
         <Route path="/register">
-          {user ? <Navigate to="/" /> : <Register />}
+          {user ? <Redirect to="/" /> : <Register />}
         </Route>
         <Route path="/profile/:username">
           <Profile />
         </Route>
-      </Routes>
+      </Switch>
     </Router>
   );
-  // return <Profile/>
 }
 
 export default App;
